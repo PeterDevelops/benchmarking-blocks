96440bce50c8d969bd81aee90f78889fdd64b352
PeterDevelops <PeterDinhBC@gmail.com>
2024-06-21 16:15:39 -0700

    add: add method for run time

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,11 +1,17 @@
 def benchmark
   # Your benchmarking code goes here.
+  start_time = Time.now
+  long_string = yield
+  end_time = Time.now
+  running_time = end_time - start_time
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
 # It's a loooong string. :)
+benchmark do
 long_string = "apple"*100000000
  
 running_time = benchmark { long_string.reverse }
  
 puts "string.reverse took #{running_time} seconds to run"
+end
